2d155c64fcc7d95d4871a139c6911d95ca281f11
amjad-a <41950386+amjad-a@users.noreply.github.com>
2018-08-07 08:13:24 +0300

    first commit

---
+++ src/helloworld.js
@@ -0,0 +1,88 @@
+import React, { component } from "react";
+
+class holloworld extends component {
+    render(){
+        return (
+            <div className="helloContainer"/>
+            
+    <body style="background-color:#999;width: 100%;height: 100%;"/>
+        <h1 style="font-size:36pt;">
+                <span style="color:orange;">Curriculum Vitae</span>
+            </h1>
+            <div style="margintop:3px; ">
+                <h1 style="font-size:36pt;">
+                    <span style="color:blue;">Amjad Abdallah Almubarak</span>
+                </h1>
+            </div>
+            <div style="float:left;width: 100%;height: 100%;"/>
+                <h1 style="font-size:25pt;">Information:</h1>
+                <div style="color:red;"/>
+                    <p>- Amjad Abdallah Almubarak </p>
+                        <br>- Saudi Arabia, Aljouf, Skaka </br>  
+                        <br> - 0563960880 </br>
+                        <br>- aalmubark0001@stu.kau.edu.sa </br>
+    
+                <p>- Assistant at Al-Jouf University, Faculty of Computing and Information Technology.</p>
+                <br> - Postgraduate Student in King Abdulaziz University. Information System Department.</br>
+            
+        
+                <h1 style="font-size:25pt;">Papers Publications:
+                </h1>
+                <table>
+                    <tr>
+                        <th>Paper Title</th>
+                        <th>Published</th>
+                        <th>Year</th>
+                    </tr> <tr>
+                    <td>Mobile Computing as a game Changer in Kingdom of Saudi Arabia Industries</td>
+                    <td>IJCA</td>
+                    <td>2015</td>
+                    </tr>
+                    <tr>
+                        <td>The security of e-health records</td>
+                        <td>IJCA</td>
+                        <td>2015</td>
+                    </tr>
+                    <tr>
+                        <td>TQM methods in Secure On-Line shopping </td>
+                        <td>IJCA</td>
+                        <td>2015</td>
+                    </tr>
+                    <tr>
+                        <td> Review of Biometric Decision Support Systems</td>
+                        <td>IJDSS</td>
+                        <td>2016</td>
+                    </tr>
+                    <tr>
+                        <td> Review of Security Approaches for Mobile Government Applications </td>
+                        <td>IJCA</td>
+                        <td>2016</td>
+                    </tr>
+                </table>
+            
+                <h1 style="font-size:15pt;">TECHNICAL EXPERIENCE:</h1>
+                <p>Languages: JAVA, HTML. Platforms: Windows Seven. Concepts: Networking, operating systems</p>
+                <h1 style="font-size:15pt;">ABILITIES:</h1>
+               <p> Skill Level MS Word, MS Excel, Power point practiced More than 1 years</p>
+                
+         
+                <h1 style="font-size:15pt;">PROFESSIONAL EXPERIENCE:</h1>
+               <p> 5 years as assistant at Aljouf University.</p>
+                <h1 style="font-size:15pt;">LANGUAGES:</h1>
+                <p>Arabic,English</p>
+               
+                <h1 style="font-size:15pt;">INTEREST:</h1>
+                <p>1.Business Process 2.Reading books</p>
+            
+                <h1 style="font-size:15pt;">REFERENCE:</h1>
+                <a href="https://www.linkedin.com/in/amjad-almubarak-452820164/">linkedin.com/in/amjad-almubarak-452820164/ </a>
+         
+         
+        
+    
+     
+        ); 
+    }
+}
+
+export default helloworld;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import helloworld from "./helloworld";
+import "./index.css";
+
+ReactDOM.render(
+ <helloworld/>,
+ document.getElementById("root")
+);
